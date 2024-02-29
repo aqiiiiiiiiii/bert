9dfe320931b72dfdb8adb7f921d93483b7def6b7
wangchen <wang.chen02@opg.cn>
2024-02-29 13:37:34 +0800

    run

--- data/download_squad_data.sh
+++ data/download_squad_data.sh
@@ -6,7 +6,5 @@ cd squad_data
 
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
-wget https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py
-#sed -i "s/:false/:False/g" evaluate-v1.1.py
-#sed -i "s/:true/:True/g" evaluate-v1.1.py
-#sed -i "s/:null/:None/g" evaluate-v1.1.py
+wget 'https://storageclwsprod1.blob.core.windows.net/bundles/0xbcd57bee090b421c982906709c8c27e1/contents.gz?se=2024-02-29T06%3A33%3A51Z&sp=rt&sv=2019-12-12&sr=b&rscd=inline%3B%20filename%3D%22evaluate-v1.1.py%22&rsce=gzip&rsct=text/x-python&sig=TgLN0KhnhYTLAMV9LqGN0ROiPOT3UCoHj1DCynht/aU%3D'
+mv index.html evaluate-v1.1.py
