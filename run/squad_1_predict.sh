ed1e4f438f25309e4896057a478338ee5ba113ee
wangchen <wang.chen02@opg.cn>
2024-02-29 11:46:12 +0800

    run

--- run/squad_1_predict.sh
+++ run/squad_1_predict.sh
@@ -3,7 +3,7 @@
 export BERT_BASE_DIR=model/uncased_L-12_H-768_A-12
 export SQUAD_DIR=data/squad_data
 
-#sed -i "s/:false/:False/g" $SQUAD_DIR/evaluate-v1.1.py
-#sed -i "s/:true/:True/g" $SQUAD_DIR/evaluate-v1.1.py
-#sed -i "s/:null/:None/g" $SQUAD_DIR/evaluate-v1.1.py
+sed -i "s/:false/:False/g" $SQUAD_DIR/evaluate-v1.1.py
+sed -i "s/:true/:True/g" $SQUAD_DIR/evaluate-v1.1.py
+sed -i "s/:null/:Null/g" $SQUAD_DIR/evaluate-v1.1.py
 python3 $SQUAD_DIR/evaluate-v1.1.py $SQUAD_DIR/dev-v1.1.json /tmp/squad_base/predictions.json
