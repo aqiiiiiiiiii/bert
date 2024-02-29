280f9705622cba06f9dc0ac90a732b1fc4e805c3
wangchen <wang.chen02@opg.cn>
2024-02-29 13:51:06 +0800

    run

--- data/download_squad_data.sh
+++ data/download_squad_data.sh
@@ -7,3 +7,7 @@ cd squad_data
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
 wget 'https://worksheets.codalab.org/rest/bundles/0xbcd57bee090b421c982906709c8c27e1/contents/blob/' -O evaluate-v1.1.py
+
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+wget https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
