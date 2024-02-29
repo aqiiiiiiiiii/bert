89d2e63afb2cfdf865447dda037842d8675ca24a
wangchen <wang.chen02@opg.cn>
2024-02-29 11:19:14 +0800

    run

--- run/squad_1_predict.sh
+++ run/squad_1_predict.sh
@@ -5,4 +5,5 @@ export SQUAD_DIR=data/squad_data
 
 sed -i "s/:false/:False/g" $SQUAD_DIR/evaluate-v1.1.py
 sed -i "s/:true/:True/g" $SQUAD_DIR/evaluate-v1.1.py
+sed -i "s/:null/:None/g" $SQUAD_DIR/evaluate-v1.1.py
 python3 $SQUAD_DIR/evaluate-v1.1.py $SQUAD_DIR/dev-v1.1.json /tmp/squad_base/predictions.json
